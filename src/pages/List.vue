eef927658a09d5437cec9cb3aeb5b1c5163f46ba
Jan Sievers <jcs@facelift-bbt.com>
2020-08-11 15:31:52 +0200

    Get data from api, service

--- src/pages/List.vue
+++ src/pages/List.vue
@@ -1,3 +1,33 @@
 <template>
-   <p>List</p>
+   <div>
+      <p>List</p>
+      <ul>
+         <li v-for="item in listItems">
+            {{ item.name.first }} {{ item.name.last }}
+         </li>
+      </ul>
+   </div>
 </template>
+
+<script>
+import * as dataService from '../services/DataService';
+export default {
+   data: () => {
+      return {
+         listItems: []
+      }
+   },
+   methods: {
+      fetchListItems() {
+         dataService.getPersons()
+         .then((res) => {
+            this.listItems = dataService.normalize(res);
+         });
+      }
+   },
+   beforeMount(){
+      this.fetchListItems()
+ },
+   
+}
+</script>
--- src/services/DataService.js
+++ src/services/DataService.js
@@ -1,1 +1,13 @@
-// Service for getting data...
+import axios from 'axios';
+import * as R from 'ramda';
+
+const apiUrl = 'https://randomuser.me/api/?results=25';
+
+export const getPersons = () => {
+    return axios.get(apiUrl);
+};
+
+export const normalize = (res) => {
+    // Extract from payload
+    return R.pathOr(['N/A'], ['data', 'results'], res);
+};
